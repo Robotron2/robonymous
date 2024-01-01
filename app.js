831808a701448365cc2813f4e0e7ef81e91eb18a
Theophilus Adeleke <theophilusadesola002@gmail.com>
2024-01-01 16:37:08 +0100

    Fix: Enables specific CORS origin.

--- app.js
+++ app.js
@@ -12,6 +12,11 @@ connectDB()
 const app = express()
 
 app.use(cors())
+app.use(function (req, res, next) {
+	res.header("Access-Control-Allow-Origin", "http://localhost:5173")
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+	next()
+})
 
 app.use(express.json())
 
